d6df51c35edd833e050cbc3fdde3dc65a5161e22
kashafdawood <kashaf.dawood@gmail.com>
2025-02-23 12:56:51 +0500

    feat: remove token refresh mechanism and related components; update candidate dashboard to fetch and display candidate data

--- app/candidate/page.tsx
+++ app/candidate/page.tsx
@@ -1,10 +1,38 @@
+"use client";
+
+import { useState } from "react";
 import { ModeToggle } from "@/components/dark-mode";
 
 export default function Dashboard() {
+  const [candidateData, setCandidateData] = useState(null);
+  const [showData, setShowData] = useState(false);
+
+  const fetchCandidate = async () => {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_URL}/api/users/candidate/me`,
+      {
+        credentials: "include",
+      }
+    );
+    const data = await res.json();
+    setCandidateData(data);
+  };
+
+  const handleClick = () => {
+    fetchCandidate();
+    setShowData(true);
+  };
+
   return (
     <>
-      <h1>Canidate Dashboard</h1>
+      <h1>Candidate Dashboard</h1>
       <ModeToggle />
+      <button onClick={handleClick}>Show Candidate Data</button>
+      {showData && candidateData && (
+        <div>
+          <p>Candidate: {candidateData}</p>
+        </div>
+      )}
     </>
   );
 }
