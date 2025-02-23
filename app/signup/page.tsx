5123352a9ff15fd22648370ba1596b7fa6afa53e
kashafdawood <kashaf.dawood@gmail.com>
2025-02-23 21:59:04 +0500

    feat: add signup page and form with validation and role selection

---
+++ app/signup/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import Image from "next/image";
+import { login } from "@/api/auth/login";
+import { useActionState } from "react";
+import Alerts from "@/components/customAlert";
+import { CheckCircle, XCircle } from "lucide-react";
+import { SignupForm } from "./signup-form";
+
+export default function LoginPage() {
+  const [state, formAction] = useActionState(login, undefined);
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-zinc-100 p-6 md:p-10 dark:bg-zinc-800">
+      {state?.message && (
+        <Alerts
+          message={state?.message}
+          variant={"success"}
+          Icon={CheckCircle}
+        />
+      )}
+      {state?.serverError && (
+        <Alerts
+          message={state?.serverError}
+          variant={"destructive"}
+          Icon={XCircle}
+        />
+      )}
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <div className="flex h-6 w-6 items-center justify-center rounded-md text-zinc-50 dark:bg-zinc-50 dark:text-zinc-900">
+            <Image src={"/StaffeeLogo.svg"} alt="Logo" width={15} height={15} />
+          </div>
+          Stafee.
+        </a>
+        <SignupForm state={state} formAction={formAction} />
+      </div>
+    </div>
+  );
+}
