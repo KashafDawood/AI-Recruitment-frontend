8c510a0ba91aaf6d6ba7a782d7c30e17e8f7e06f
kashafdawood <kashaf.dawood@gmail.com>
2025-02-27 06:27:57 +0500

    feat: implement EmployerLayout with sidebar navigation and add nav items

---
+++ app/employer/nav.ts
@@ -0,0 +1,21 @@
+"use client";
+
+import { Briefcase, BookmarkCheck, CheckCircle } from "lucide-react";
+
+export const navItems = [
+  {
+    title: "Jobs",
+    url: "/employer/jobs",
+    icon: Briefcase,
+  },
+  {
+    title: "candidate",
+    url: "/employer/candidates",
+    icon: BookmarkCheck,
+  },
+  {
+    title: "Add a Blog",
+    url: "/employer/Blogposts",
+    icon: CheckCircle,
+  },
+];
