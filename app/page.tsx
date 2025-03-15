36ceb6e3ec8fecf12f8351983c221c28fa31fdae
kashafdawood <kashaf.dawood@gmail.com>
2025-03-15 06:46:52 +0500

    feat: refactor HeroSection component to integrate HeroHeader and enhance layout structure

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,12 @@ import About from "../components/home/About";
 import Features from "../components/home/Features";
 import HowItWorks from "../components/home/HowItWorks";
 import Blogs from "../components/home/Blogs";
-import Contact from "../components/home/Contact";
+import Contact from "../components/home/Footer";
 import { JSX } from "react";
-import { NavMenu } from "@/components/home/navbar";
 
 export default function Home(): JSX.Element {
   return (
     <div>
-      <NavMenu />
       <HeroSection />
       <About />
       <Features />
