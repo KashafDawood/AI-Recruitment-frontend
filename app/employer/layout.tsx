b0763d8e414510ad77cbd3ba8e127727f1d9c056
kashafdawood <kashaf.dawood@gmail.com>
2025-03-21 09:22:32 +0500

    feat: add responsive sidebar functionality to candidate and employer layouts

--- app/employer/layout.tsx
+++ app/employer/layout.tsx
@@ -1,15 +1,31 @@
+"use client";
+
 import { AppSidebar } from "@/components/sidebar/app-sidebar";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { navItems } from "./nav";
 import { DashboardNav } from "@/components/nav/dashboard-nav";
+import { useEffect, useState } from "react";
 
 export default function CandidateLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 1024) {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <SidebarProvider>
+    <SidebarProvider open={isOpen} onOpenChange={setIsOpen}>
       <AppSidebar data={navItems} />
       <div className="flex flex-col flex-1">
         <DashboardNav />
